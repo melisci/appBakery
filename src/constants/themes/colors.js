a5a0a51bd205c0c1f037d94b555bf215c14b573b
melisci <melisascime@gmail.com>
2022-10-09 18:48:38 -0300

    #4 styles and colors

---
+++ src/constants/themes/colors.js
@@ -0,0 +1,9 @@
+export const colors ={
+    primary: "#6C464F",
+    secondary: "#9E768F",
+    background: '#F0F0F0',
+    text: '#000000',
+    black: '#000000',
+    white: '#FFFFFF',
+    gray: '#C4C4C4',
+}
--- src/navigation/shop.js
+++ src/navigation/shop.js
@@ -1,14 +1,37 @@
 import React from 'react';
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Categories, Products, Product } from "../screens";
+import { isAndroid } from "../utils";
+import { colors } from "../constants/themes";
+import { Text } from "react-native";
 
 const Stack = createNativeStackNavigator();
 
 
 const ShopNavigator = () => {
     return(
-        <Stack.Navigator initialRouteName="Categories">
-            <Stack.Screen name="Categories" component={Categories} />
+        <Stack.Navigator 
+        initialRouteName="Categories"
+        screenOptions={{
+            headerStyle: {
+                backgroundColor: isAndroid ? colors.primary : colors.secondary,
+            },
+            headerTintColor: colors.text,
+                headerTitleStyle: {
+                    fontFamily: 'OpenSans-Bold',
+                },
+            
+            }}
+        
+        >
+            <Stack.Screen 
+            name="Categories" 
+            component={Categories} 
+            options={{
+                title: 'Categorías',
+            }}
+            
+            />
             <Stack.Screen name="Products" component={Products} />
             <Stack.Screen name="Product" component={Product} />
         
