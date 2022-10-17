d9ec0218f048f0cfc4dc9d0676f853ef1219af13
melisci <melisascime@gmail.com>
2022-10-17 12:05:29 -0300

    firebase redux

---
+++ src/constants/firebase/index.js
@@ -0,0 +1,1 @@
+export const URL_API = 'https://appbakery-48e4f-default-rtdb.firebaseio.com/'
--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
-import { createStore, combineReducers} from "redux"
+import { createStore, combineReducers, applyMiddleware} from "redux"
+import {thunk} from "redux-thunk"
 
 import { productsReducer, categoryReducer} from "./reducers";
 
@@ -8,4 +9,4 @@ const rootReducer = combineReducers({
 
 });
 
-export default createStore (rootReducer);
+export default createStore (rootReducer, applyMiddleware(thunk));
